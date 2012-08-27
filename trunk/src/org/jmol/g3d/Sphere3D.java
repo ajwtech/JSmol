601877d8372c516c1035c0073ec86b2c2fe51a7e
hansonr <hansonr@6e7add11-07ff-4fc6-9666-6c75ffb6cef5>
2012-08-27 02:19:35 +0000

    first integration Jmol-->JSmol

---
+++ trunk/src/org/jmol/g3d/Sphere3D.java
@@ -0,0 +1,10 @@
+package org.jmol.g3d;
+
+class Sphere3D {
+
+	public static void flushSphereCache() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ trunk/src/org/jmol/i18n/GT.java
@@ -0,0 +1,40 @@
+package org.jmol.i18n;
+
+public class GT {
+
+	public GT(String value) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static String getLanguage() {
+		return "en_US";
+	}
+
+	public static String _(String string, int abs) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static boolean getDoTranslate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static void setDoTranslate(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static String _(String string) {
+		return string;
+	}
+
+	public static String _(String string, String localName) {
+		return string;
+	}
+
+	public static String _(String string, Object[] objects) {
+		return string;
+	}
+
+}
