cc5a27da154300cd84ba016f0ae810d0e3900c4b
hansonr <hansonr@6e7add11-07ff-4fc6-9666-6c75ffb6cef5>
2018-09-10 15:11:51 +0000

    fix for popup menu not actuating checkbox items. single-file replacement is dist/tmp/coremenu.z.js  and dist/tmp/corejsvmenu.z.js, which both go into j2s/core

--- trunk/jsmoljs/JSmolJavaExt.js
+++ trunk/jsmoljs/JSmolJavaExt.js
@@ -590,8 +590,8 @@ Clazz.overrideConstructor(Float, function(v){
 });
 
 Float.serialVersionUID=Float.prototype.serialVersionUID=-2671257302660747028;
-Float.MIN_VALUE=Float.prototype.MIN_VALUE=3.4028235e+38;
-Float.MAX_VALUE=Float.prototype.MAX_VALUE=1.4e-45;
+Float.MIN_VALUE=Float.prototype.MIN_VALUE=1.4e-45;
+Float.MAX_VALUE=Float.prototype.MAX_VALUE=3.4028235e+38;
 Float.NEGATIVE_INFINITY=Number.NEGATIVE_INFINITY;
 Float.POSITIVE_INFINITY=Number.POSITIVE_INFINITY;
 Float.NaN=Number.NaN;
